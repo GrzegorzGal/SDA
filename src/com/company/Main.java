2ce488d53970c82ec7af7ccf74d247e47831884e
Grzegorz Galicki <1grzegorz.galicki@gmail.com>
2019-01-19 15:44:39 +0100

    zmina do wrzucenia na serwer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
             System.out.println("hello teskt zrobione na feature/2");
-            System.out.println("hello dodany w feature1 dolozenie na hub zrobione na futhure1 lokalnie");
+            System.out.println("hello dodany w commit czy działa automatycznie z commitem");
 
 
     }
