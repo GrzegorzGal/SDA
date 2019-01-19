05a64a1a42a05c32b62bca1a4518fd5450e146aa
Grzegorz Galicki <1grzegorz.galicki@gmail.com>
2019-01-19 15:37:41 +0100

    zmina do wrzucenia na serwer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
             System.out.println("hello teskt zrobione na feature/2");
-            System.out.println("hello dodany w feature1");
+            System.out.println("hello dodany w feature1 zmian lokalna na serwer");
 
 
     }
